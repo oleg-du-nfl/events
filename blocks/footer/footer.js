7f11a4c53ca88b4c730c0bfce835dfd449fe0548
oleg-du-nfl <oleg.durmanov@nfl.com>
2026-07-02 11:43:18 -0400

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,11 +1,18 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
+import { getSiteConfig } from '../../scripts/config.js';
 
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
+  // Apply footer background color from site config
+  const config = await getSiteConfig();
+  if (config['footer-background']) {
+    document.querySelector('footer').style.backgroundColor = config['footer-background'];
+  }
+
   // load footer as fragment
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
@@ -15,6 +22,5 @@ export default async function decorate(block) {
   block.textContent = '';
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
-
   block.append(footer);
 }
